21d3be9dc6aa47ed58504f490b6782d09d04f03a
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2019-12-20 17:28:38 +0800

    remove redundant lines

--- tests/test_wallpaper.py
+++ tests/test_wallpaper.py
@@ -2,17 +2,13 @@ import sys
 from os.path import abspath, relpath, dirname
 sys.path.append(abspath(relpath('..', dirname(__file__))))
 
-import sys
-from os.path import abspath, relpath, dirname
-sys.path.append(abspath(relpath('..', dirname(__file__))))
-
 from xml_params import get_bg_properties_xml_header, Wallpaper, WallpaperImage
 from xml_print import generate_xml_element, print_indented
 
 
 def test_wallpaper():
     print(get_bg_properties_xml_header())
-    
+
     sample_xml = Wallpaper('/usr/share/backgrounds/contest/eoan.xml', name='Ubuntu 19.10 Community Wallpapers')
     sample_image = WallpaperImage('/usr/share/backgrounds/Beijling_park_burial_path_by_Mattias_Andersson.jpg')
 
